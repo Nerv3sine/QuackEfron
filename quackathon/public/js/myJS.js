d9db2310a876a756366028da3d5f8cdeb9d4f90c
Ikenna Okpala <ijo1@sfu.ca>
2022-02-12 14:44:53 -0800

    Add start game and end game feature

--- quackathon/public/js/myJS.js
+++ quackathon/public/js/myJS.js
@@ -27,42 +27,32 @@ var ROW = 5;
 var COLUMN = 5;
 var POINTS = 100;
 var MAX_GUESS = 9;
+var randLimit = 10;
 
 var count = 0;
 var score = 0;
 var guess = 0;
 cardList = []
 cardObjList = []
+isGameOn = true;
 
 for(let x = 0; x < ROW; x++){
     for(let y = 0; y < COLUMN; y++){
-
-        // generates random duck cards
-        random = Math.floor(Math.random() * ROW * COLUMN)
 
         const card = document.createElement("button")
         card.classList.add("btn")
         card.classList.add("btn-primary")
 
-       // card.innerHTML = count
+        card.innerHTML = count
 
         
-
         // saves HTML elment for tracking
         cardList.push(card);
 
-        //creates Card object
-        cardObj = new Card(false, false)
-
-        // randomly assigns ducks
-        if(random == 0)
-        {
-            cardObj.duckify();
-            card.innerHTML = "duck"
-        }
-
         let func = "flipCard(" + count + ")"
         card.setAttribute("onclick", func)
+
+        cardObj = new Card(false, false)
 
         cardObjList.push(cardObj)
 
@@ -70,46 +60,65 @@ for(let x = 0; x < ROW; x++){
         count++
     }
     grid.appendChild(document.createElement("br"))
+
+    startGame();
 }
 
+function startGame()
+{
+    for(let i  = 0; i < cardList.length; i++)
+    {
+        // generates random duck cards
+        random = Math.floor(Math.random() * ROW * COLUMN)
+
+        // randomly assigns ducks
+        if(random % randLimit == 0)
+        {
+            cardObjList[i].duckify();
+            cardList[i].innerHTML = "d"
+        }
+    }
+    
+}
 
 function flipCard(e)
 {
-    cardObj = cardObjList[e]
-
-    if(cardObj.isFlipped)
+    if(isGameOn)
     {
-        console.log("Flipped")
-        return
+        cardObj = cardObjList[e]
+
+        if(cardObj.isFlipped)
+        {
+            console.log("Flipped")
+            return
+        }
+
+        // if the card has a duck
+        if(cardObj.hasDuck)
+        {
+            score+= POINTS;
+        }
+        else
+        {
+            console.log("No duck")
+        }
+
+        guess++;
+        if(guess > MAX_GUESS)
+        {
+            endGame()
+        }
+
+        cardObj.flip()
+
     }
-
-    // if the card has a duck
-    if(cardObj.hasDuck)
-    {
-        score+= POINTS;
-    }
-    else
-    {
-        console.log("No duck")
-    }
-
-    guess++;
-    if(guess > MAX_GUESS)
-    {
-        endGame()
-    }
-
-    cardObj.flip()
-
-    //test
-    console.log("Score: "+score);
-    console.log("Guess:"+guess);
-
+    console.log("Score:"+score)
+    console.log("Guess:"+guess)
 }
 
 function endGame()
 {
-    console.log("Gamed ended")
+    isGameOn = false;
 }
 
 

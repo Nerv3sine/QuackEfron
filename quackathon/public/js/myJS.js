f80ac29fd4b2c32167855f96fd38da880efc6d78
Ikenna Okpala <ijo1@sfu.ca>
2022-02-12 14:02:15 -0800

    Add flipcard, random  and other features

--- quackathon/public/js/myJS.js
+++ quackathon/public/js/myJS.js
@@ -1,19 +1,71 @@
 
+class Card
+{
+    constructor(isFlipped, hasDuck)
+    {
+        this.isFlipped = isFlipped;
+        this.hasDuck = hasDuck;
+    }
+
+    flip()
+    {
+        this.isFlipped = true;
+    }
+
+    duckify()
+    {
+       this.hasDuck = true;
+    }
+
+
+
+}
 
 let grid = document.getElementById("board")
 
+var ROW = 5;
+var COLUMN = 5;
+var POINTS = 100;
+var MAX_GUESS = 9;
 
 var count = 0;
+var score = 0;
+var guess = 0;
 cardList = []
+cardObjList = []
 
-for(let x = 0; x < 5; x++){
-    for(let y = 0; y < 5; y++){
+for(let x = 0; x < ROW; x++){
+    for(let y = 0; y < COLUMN; y++){
+
+        // generates random duck cards
+        random = Math.floor(Math.random() * ROW * COLUMN)
+
         const card = document.createElement("button")
         card.classList.add("btn")
         card.classList.add("btn-primary")
+
         card.innerHTML = count
-        let cardTemp = "flipCard()"
-        card.setAttribute("onclick", cardTemp)
+
+        
+
+        // saves HTML elment for tracking
+        cardList.push(card);
+
+        //creates Card object
+        cardObj = new Card(false, false)
+
+        // randomly assigns ducks
+        if(random == 0)
+        {
+            cardObj.duckify();
+        }
+
+        card.addEventListener("click", function(e) {flipCard(e)})
+
+        att = card.setAttribute("index")
+        att.value = ""+count
+
+        cardObjList.push(cardObj)
 
         grid.appendChild(card)
         count++
@@ -21,9 +73,45 @@ for(let x = 0; x < 5; x++){
     grid.appendChild(document.createElement("br"))
 }
 
-function flipCard()
+
+
+function flipCard(e)
 {
-    console.log("Works")
+    cardObj = cardObjList[parseInt(e.target.getAttribute("index"))]
+
+    if(cardObj.isFlipped)
+    {
+        console.log("Flipped")
+        return
+    }
+
+    // if the card has a duck
+    if(cardObj.hasDuck)
+    {
+        score+= POINTS;
+    }
+    else
+    {
+        console.log("No duck")
+    }
+
+    guess++;
+    if(guess > MAX_GUESS)
+    {
+        endGame()
+    }
+
+    cardObj.flip()
+
+    //test
+    console.log("Score: "+score);
+    console.log("Guess:"+guess);
+
+}
+
+function endGame()
+{
+    console.log("Gamed ended")
 }
 
 
